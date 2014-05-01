62c4c07b101ba8a74608a0c07e381bd906872bed
Bjoern Bastian <basbjo@gmail.com>
2014-05-01 17:28:08 +0200

    fileinfo: Get number of data columns in files
    The number of columns minus IF_FUTURE is determined once
    and then saved to variables NCOLS_file to be used again.

--- makefiles/fileinfo.mk
+++ makefiles/fileinfo.mk
@@ -1,18 +1,31 @@
-.PHONY:
+.PHONY: showncols
 
 ## default settings
 
 # settings/data to be shown by showconf/showdata
-SHOWCONF +=
+SHOWCONF += IF_FUTURE
 SHOWDATA +=
 
 ## default settings that must be changed before including this file
 
 ## variables
+# maximum number of columns per file
+lastcol = $(call getmax,$(foreach file,${DATA},${NCOLS_${file}}))
 
 ## rules
+showncols: ;@true
+	$(info # Number of columns in file minus IF_FUTURE (${IF_FUTURE}))
+	$(foreach file,${DATA},$(info ${file}: ${NCOLS_${file}}))
 
 ## macros
+# columns in file $(1) minus $(IF_FUTURE)
+define NCOLS_template
+  NCOLS_$(1) = $(call fcols,${1})
+endef
 
 ## macros to be called later
-MACROS +=
+MACROS += fileinfo
+
+define fileinfo
+  $(foreach file,${DATA},$(eval $(call NCOLS_template,${file})))
+endef
