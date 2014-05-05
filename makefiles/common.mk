3c385ef5a7c093595a91b0eeec7d9a1019fc147f
Bjoern Bastian <basbjo@gmail.com>
2014-05-05 21:46:51 +0200

    Create list of data files in remote directories
    Two lists are created: paths to data files and the corresponding link
    names in the current directory.  The default source directory is the
    parent of the makefile directory.  A suffix may be omitted in link
    names.  Wildcards are allowed and only existing files are considered.

--- makefiles/common.mk
+++ makefiles/common.mk
@@ -4,11 +4,16 @@ all: $$(all)
 
 ## default variables
 SCR ?= $(makedir)/scripts# scripts directory
+datadirs ?= $(prefix)# remote data directories
+DROPSUFFIX ?= # data filename suffix to be omitted in link names
 
 ## common variables
 
 ## source data files
-DATA += $(wildcard ${DATA_HERE})
+DATA += $(sort $(wildcard ${DATA_HERE}) ${DATALINKS})#without repetitions
+REMOTEDATA += $(foreach wildcard,${DATA_LINK},$(foreach dir,${datadirs},\
+	      $(wildcard ${dir}/${wildcard})))
+DATALINKS = $(notdir $(patsubst %${DROPSUFFIX},%,${REMOTEDATA}))
 
 ## common phony targets
 .PHONY: all
