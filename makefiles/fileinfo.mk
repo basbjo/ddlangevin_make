47795130ebec705629db14564243c3903b6223dd
Bjoern Bastian <basbjo@gmail.com>
2014-05-08 23:05:15 +0200

    fileinfo: Basic structure

---
+++ makefiles/fileinfo.mk
@@ -0,0 +1,18 @@
+.PHONY:
+
+## default settings
+
+# settings/data to be shown by showconf/showdata
+SHOWCONF +=
+SHOWDATA +=
+
+## default settings that must be changed before including this file
+
+## variables
+
+## rules
+
+## macros
+
+## macros to be called later
+MACROS +=
