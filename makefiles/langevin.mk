563884172a7f9e08665708ce8e763ad2ffb072a3
Bjoern Bastian <bjoern.bastian@physik.uni-freiburg.de>
2014-05-10 15:18:50 +0200

    langevin: Links to dpca data and column selection
    To get a data file with fewer columns, say n=3, rules to generate
    »file.3cols« et cetera are provided.  The first n columns and, if
    needed, the follower column which is the last column, are chosen.

--- makefiles/langevin.mk
+++ makefiles/langevin.mk
@@ -3,7 +3,7 @@
 ## default settings
 
 # settings/data to be shown by showconf/showdata
-SHOWCONF +=
+SHOWCONF += IF_FUTURE
 SHOWDATA +=
 
 ## default settings that must be changed before including this file
@@ -11,14 +11,26 @@ SHOWDATA +=
 ## variables
 
 ## rules
+define template_selectcolumns
+$(1).%cols : $(1)
+	$(SCR)/select_columns.awk -vnfirst=$$* -vnlast=$$(IF_FUTURE) $$< > $$@
+endef
+
+define rule_langevin
+$(foreach file,${DATA},$(eval $(call template_selectcolumns,${file})))
+endef
 
 ## macros to be called later
-MACROS +=
+MACROS += rule_langevin
 
 ## info
 ifndef INFO
 INFO =
 define INFOADD
+
+To extract say 3 columns from a data file, call »make file.3cols«.
+If IF_FUTURE is 1, also the last column of the file is appended.
+
 endef
 else
 INFOend +=
