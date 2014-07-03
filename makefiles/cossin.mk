063782db8d55ced36efc26060b3f7c8059975ec3
Bjoern Bastian <bjoern.bastian@physik.uni-freiburg.de>
2014-07-03 18:16:37 +0200

    cossin: Generate cos-/sin-transformed data

--- makefiles/cossin.mk
+++ makefiles/cossin.mk
@@ -1,28 +1,37 @@
-.PHONY:
+.PHONY: cossin
+cossin: $$(COSSINDATA)
 
 ## default settings
 
 # settings/data to be shown by showconf/showdata
-SHOWCONF +=
+SHOWCONF += DIH_MIN_COL DIH_MAX_COL
 SHOWDATA +=
 
 ## default settings that must be changed before including this file
 
 ## variables
+# cos-/sin-transformed dihedral angles
+COSSINDATA += $(addsuffix .cossin,${DATA})
 
 ## rules
+%.cossin : %
+	# cos-/sin-transform of inner dihedrals in $<
+	$(SCR)/cos_sin_tran.awk\
+		-v dih_min_col=$(strip ${DIH_MIN_COL})\
+		-v dih_max_col=$(strip ${DIH_MAX_COL}) $< > $@
 
 ## macros to be called later
 #MACROS +=
 
 ## info
 ifndef INFO
-INFO =
+INFO = cossin
 define INFOADD
 endef
 else
-INFOend +=
+INFOend += cossin
 endif
+INFO_cossin = create cos-/sin-transformed data
 
 ## keep intermediate files
 PRECIOUS +=
@@ -30,4 +39,4 @@ PRECIOUS +=
 ## clean
 PLOTS_LIST +=
 CLEAN_LIST +=
-PURGE_LIST +=
+PURGE_LIST += $(COSSINDATA)
