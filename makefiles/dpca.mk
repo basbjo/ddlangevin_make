fcc0ff1b942f72f4c478ffe893d3501c0df02612
Bjoern Bastian <bjoern.bastian@physik.uni-freiburg.de>
2014-07-03 18:31:29 +0200

    dpca: Use cossin to get cos-/sin-transformed data

--- makefiles/dpca.mk
+++ makefiles/dpca.mk
@@ -1,23 +1,17 @@
-.PHONY: cossin dpca plot
+.PHONY: dpca plot
 dpca: $$(PCADATA)
-
-cossin: $$(COSSINDATA)
 
 plot: dpca $$(CVARPLOT)
 
 ## default settings
-PCA_FUTURE ?= $(or ${IF_FUTURE},0)
-# if 1: append last column of source data to dpca data
 
 # settings/data to be shown by showconf/showdata
-SHOWCONF += DIH_MIN_COL DIH_MAX_COL PCA_FUTURE
-SHOWDATA +=
+SHOWCONF +=
+SHOWDATA += PCADATA
 
 ## default settings that must be changed before including this file
 
 ## variables
-# cos-/sin-transformed dihedral angles
-COSSINDATA += $(addsuffix .cossin,${DATA})
 # projected data from principal component analysis
 PCADATA += $(addsuffix .pca,${COSSINDATA})
 # suffix for projected data that is further analysed
@@ -28,27 +22,12 @@ CVARPLOT = $(addsuffix .eigval.png,${COSSINDATA})
 # minima and maxima as reference for ranges
 MINMAXALL = $(PCADATA)
 
-## rules
-%.cossin : %
-	# cos-/sin-transform of inner dihedrals in $<
-	$(SCR)/cos_sin_tran.awk\
-		-v dih_min_col=$(strip ${DIH_MIN_COL})\
-		-v dih_max_col=$(strip ${DIH_MAX_COL}) $< > $@
-
 %$(PROJSUFFIX).tmp : %.cossin
 	# perform principal component analysis on $<
 	name=$<; $(FASTCA) -f $$name -p $@ -c $$name.cov -v $$name.eigvec -V $$name.eigval
 
 %$(PROJSUFFIX) : %$(PROJSUFFIX).tmp
 	$(appendlastcol_command)
-
-define appendlastcol_command
-  $(if $(shell [ ${PCA_FUTURE} -eq 1 ] && echo yes),\
-	  $(info # write result with follower column to $@)\
-	  awk '!/^#/{print $$NF}' $* | paste -d\  $< - > $@,\
-	  $(info # move result to $@)\
-	  mv $< $@)
-endef
 
 %.eigval.tex : %.pca $(SCR)/plot_cumulative.py
 	$(SCR)/plot_cumulative.py $(basename $@)
@@ -59,7 +38,6 @@ endef
 ## info
 ifndef INFO
 INFO = cossin dpca plot clean
-INFO_cossin = create cos-/sin-transformed data
 INFO_dpca   = dihedral principal component analysis
 INFO_plot   = plot cumulative variances
 INFO_clean  = delete cos-/sin-transformed data
@@ -68,6 +46,9 @@ endef
 else
 INFOend +=
 endif
+
+## makefile includes (must remain after info)
+include $(makedir)/cossin.mk
 
 ## keep intermediate files
 PRECIOUS += $(COSSINDATA)
