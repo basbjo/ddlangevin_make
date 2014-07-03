8029b2388abd4f79c1a7c1943d3389c6052f658f
Bjoern Bastian <bjoern.bastian@physik.uni-freiburg.de>
2014-07-03 18:23:56 +0200

    cossin: Command to append follower column to data
    A common command is defined to append the last column of source data to
    projected data. It is only used in other makefiles that include cossin.

--- makefiles/cossin.mk
+++ makefiles/cossin.mk
@@ -2,9 +2,12 @@
 cossin: $$(COSSINDATA)
 
 ## default settings
+PROJ_FUTURE ?= $(or ${IF_FUTURE},0)
+# if 1: append last column of source data to projected data
+# this is only used in other makefiles that include this one
 
 # settings/data to be shown by showconf/showdata
-SHOWCONF += DIH_MIN_COL DIH_MAX_COL
+SHOWCONF += DIH_MIN_COL DIH_MAX_COL PROJ_FUTURE
 SHOWDATA +=
 
 ## default settings that must be changed before including this file
@@ -19,6 +22,16 @@ COSSINDATA += $(addsuffix .cossin,${DATA})
 	$(SCR)/cos_sin_tran.awk\
 		-v dih_min_col=$(strip ${DIH_MIN_COL})\
 		-v dih_max_col=$(strip ${DIH_MAX_COL}) $< > $@
+
+# common command to append follower column to projected data
+# this is only used in other makefiles that include this one
+define appendlastcol_command
+  $(if $(shell [ ${PROJ_FUTURE} -eq 1 ] && echo yes),\
+	  $(info # write result with follower column to $@)\
+	  awk '!/^#/{print $$NF}' $* | paste -d\  $< - > $@,\
+	  $(info # move result to $@)\
+	  mv $< $@)
+endef
 
 ## macros to be called later
 #MACROS +=
