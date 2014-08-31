information: Calculation of negentropy fixed
Subtract estimated entropy from the exact entropy of a Gaussian with the
same variance to obtain the estimated negentropy.  Before, a Gaussian
with the same variance centered with respect to the histogram ranges was
cut off at the range boarders and binned in the same histogram edges.
This procedure generates a density much more similar to the original
histogram than to a Gaussian function and underestimates the negentropy.

makefiles/scripts/calc_negentropy.py
@@ -4,39 +4,6 @@
 import sys, argparse
 import numpy as np
 
-def gaussian(x, mu, var):
-    """
-    Gaussian function with expected value mu and variance var.
-    The function is not normalized to a probability density.
-
-    :Returns: Function value at x.
-    """
-    return np.exp(-np.power(x - mu, 2.) / (2 * var))
-
-def bingaussianentropy(var, bin_edges):
-    """
-    Calculates Shannon entropy of a Gaussian function with variance var
-    that is centered to the mean of the bin centers.  The function values
-    are evaluted at the bin centers and the entropy is estimated from the
-    resulting histogram.
-
-    :Returns: Tuple (entropy, mean).
-    """
-    binwidths = np.diff(bin_edges)
-    bincenters = bin_edges[:-1] + 0.5*binwidths
-    mu = np.mean(bincenters)
-    # binned gaussian distribution
-    gaussbinned = gaussian(bincenters, mu, var)
-    # normalize binned gaussian distribution
-    gaussbinned = gaussbinned/np.sum(gaussbinned)/binwidths
-    # take only non-zero elements for logarithm
-    gaussbinned = gaussbinned[np.nonzero(gaussbinned)]
-    # Shannon entropy
-    entropygaussbinned = -np.dot(np.log(gaussbinned),
-            (binwidths*gaussbinned).T)
-
-    return entropygaussbinned, mu
-
 def negentropy(data, nrbins):
     """
     Calculate negentropy of data by calculating a histogram with nrbins and
@@ -47,24 +14,21 @@ def negentropy(data, nrbins):
     hist, bin_edges = np.histogram(data, bins=nrbins, density=True)
     binwidths = np.diff(bin_edges)
     bincenters = bin_edges[:-1] + 0.5*binwidths
-    # variance: delta X * sum_i(x_i**2*f(x_i))
-    var = np.dot(hist*binwidths, bincenters**2)
+    variance = np.var(data, ddof=1)
 
-    # only non-zero nrbins of data distribution
+    # only non-zero nrbins of data distribution before taking logarithm
     ifnonzero = np.nonzero(hist)
     posbinwidths = binwidths[ifnonzero]
     histpos = hist[ifnonzero]
     # normalize distribution
     histpos /= np.sum(histpos)
     # entropy of binned distribution
-    entropy = -np.dot(np.log(histpos/posbinwidths), histpos.T)
-    # entropy of binned Gaussian function
-    entropygaussbinned, mu = bingaussianentropy(var, bin_edges)
+    entropy = -np.dot(np.log(histpos/posbinwidths), histpos)
 
     # exactly integrated entropy of a Gaussian function
-    #entropygauss = np.log(2*np.pi*np.e*var)/2
+    entropygauss = np.log(2*np.pi*np.e*variance)/2
 
-    return entropygaussbinned-entropy, entropygaussbinned, entropy
+    return entropygauss-entropy, entropygauss, entropy
 
 
 #######
@@ -119,12 +83,12 @@ if __name__ == "__main__":
             curdat /= sd
             sys.stderr.write("stddev before whitening: %f and after: %f\n" %
                     (sd, np.std(curdat)))
-        outlist += [[nr+1]+list(negentropy(curdat, args.nrbins))]
+        outlist.append([nr+1]+list(negentropy(curdat, args.nrbins)))
 
     # write out results
     if args.outfile == None:
             args.outfile = sys.stdout
-    np.savetxt(args.outfile, outlist, fmt=('%d' + (len(outlist[0])-1)*' %9.5e'),
+    np.savetxt(args.outfile, outlist, fmt=('%d' + (len(outlist[0])-1)*' %e'),
             header=("component negentropy entropy_of_gaussian entropy_of_system"
                 +" (%d bins)" % args.nrbins))
 