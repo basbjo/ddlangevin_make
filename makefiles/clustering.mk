d5a5742539ac9c2095571012ff4408a282f998e9
Bjoern Bastian <bjoern.bastian@physik.uni-freiburg.de>
2014-10-06 15:50:27 +0200

    clustering: Geometric clustering in Ramachandran plot
    The clustering script is adjusted to the molecule AIB9.

--- makefiles/clustering.mk
+++ makefiles/clustering.mk
@@ -1,28 +1,40 @@
-.PHONY:
+.PHONY: clustering
+clustering: $$(CLUSTER_TRAJ)
 
 ## default settings
 
 # settings/data to be shown by showconf/showdata
-SHOWCONF +=
-SHOWDATA +=
+SHOWCONF += CLUSTER_SCRIPT
+SHOWDATA += CLUSTER_TRAJ
 
 ## default settings that must be changed before including this file
 
 ## variables
+CLUSTER_SCRIPT ?= $(SCR)/clustering_aib.awk
+CLUSTER_TRAJ = $(addsuffix .clutraj,${DATA})
 
 ## rules
+%.clutraj : % $(CLUSTER_SCRIPT)
+	$(clustering_command)
+
+define clustering_command
+$(lastword $+)\
+	-v min_col=$(strip ${MIN_COL})\
+	-v max_col=$(strip ${MAX_COL}) $< > $@
+endef
 
 ## macros to be called later
 #MACROS +=
 
 ## info
 ifndef INFO
-INFO =
+INFO = clustering
 define INFOADD
 endef
 else
 INFOend +=
 endif
+INFO_clustering = apply clustering
 
 ## keep intermediate files
 PRECIOUS +=
@@ -30,4 +42,4 @@ PRECIOUS +=
 ## clean
 PLOTS_LIST +=
 CLEAN_LIST +=
-PURGE_LIST +=
+PURGE_LIST += $(CLUSTER_TRAJ)
---
+++ makefiles/scripts/clustering_aib.awk
@@ -0,0 +1,42 @@
+#!/usr/bin/awk -f
+#Geometric clustering for AIB in Ramachandran plot
+BEGIN {
+	script="clustering.awk"
+}
+!/^#/ {
+	sum = 1
+	unknown = 0
+	for(i=min_col;i<=max_col;i+=2)
+	{
+		if(($i+50)*($i+50)+($(i+1)+50)*($(i+1)+50)<1000)
+		{
+			color=1
+		}
+		else if(($i-50)*($i-50)+($(i+1)-50)*($(i+1)-50)<1000)
+		{
+			color=2
+		}
+		else
+		{
+			unknown = 1
+		}
+		sum += (color-1)*2**int((i-min_col)/2)
+	}
+	if (unknown == 0)
+	{
+	    print sum
+	}
+	else
+	{
+	    print 0
+	}
+	if(! (NR % 10000))
+	{
+		printf("\r%s: lines processed: %d", script, NR) > "/dev/stderr"
+	}
+}
+END {
+    if(NR>=10000) {
+	    printf("\n") > "/dev/stderr"
+    }
+}
