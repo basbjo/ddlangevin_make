ffc23154ac7a47e165fdf4be489c63118b14690e
Bjoern Bastian <bjoern.bastian@physik.uni-freiburg.de>
2014-10-10 17:12:23 +0200

    fields: Target creation for binning of fields

--- makefiles/fields.mk
+++ makefiles/fields.mk
@@ -22,13 +22,33 @@ head $(1) | grep '^#x1' | sed 's/ [01]$$//' \
 	| nl -ba -s\  | sed 's/^  *//'
 endef
 
+getfieldno_macro = $(showfields_macro) | grep $(2) | cut -d\  -f1
+
+define template_field
+$(1).x% : $(1)
+	$$(eval nums := $$(foreach label,$$(subst ., ,x$$*),\
+		$$(shell $$(call getfieldno_macro,${1},$${label}))))
+	$$(if $$(patsubst 2,,$$(words $${nums}))\
+		,,binning)$$(if $$(patsubst 3,,$$(words $${nums}))\
+		,,binning2d) -V0\
+		-c $$(shell echo $${nums} | tr ' ' ',') $$< -o $$@
+endef
+
 ## macros to be called later
-#MACROS +=
+MACROS += rule_fields
+
+define rule_fields
+$(foreach file,${DATA},$(eval $(call template_field,${file})))
+endef
 
 ## info
 ifndef INFO
 INFO = showfields
 define INFOADD
+
+To calculate field binnings of a »file« use the labels that are shown
+by target »showfields«, e.g. »make file.x1.f1« or »make file.x1.x2.f2«.
+
 endef
 else
 INFOend +=
