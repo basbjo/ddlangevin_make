f6093af5100e79bd4e155f5970f213262d94c6a4
Bjoern Bastian <bjoern.bastian@physik.uni-freiburg.de>
2014-10-14 12:11:10 +0200

    pca: Principal component analysis with fastca

--- makefiles/pca.mk
+++ makefiles/pca.mk
@@ -1,33 +1,73 @@
-.PHONY:
+.PHONY: select pca plot
+select: $$(INNERCOLSDATA)
+
+pca: $$(PCADATA)
+
+plot: pca $$(CVARPLOT)
 
 ## default settings
 
 # settings/data to be shown by showconf/showdata
 SHOWCONF +=
-SHOWDATA +=
+SHOWDATA += PCADATA
 
 ## default settings that must be changed before including this file
 
 ## variables
+# selected inner columns
+INNERCOLSDATA += $(addsuffix .ic,${DATA})
+# projected data from principal component analysis
+PCADATA += $(addsuffix .pca,${INNERCOLSDATA})
+# suffix for projected data that is further analysed
+PROJSUFFIX = .ic.pca
+PROJDROPSUFFIX =# drop this suffix in subdirs
+# plot of cumulative variances (eigenvalues)
+CVARPLOT = $(addsuffix .eigval.png,${INNERCOLSDATA})
+# minima and maxima as reference for ranges
+MINMAXALL = $(PCADATA)
 
 ## rules
+%.ic : %
+	# selection of inner columns in $<
+	$(SCR)/select_inner_columns.awk\
+		-v dih_min_col=$(strip ${DIH_MIN_COL})\
+		-v dih_max_col=$(strip ${DIH_MAX_COL}) $< > $@
+
+%$(PROJSUFFIX).tmp : %.ic
+	# perform principal component analysis on $<
+	name=$<; $(FASTCA) -f $$name -p $@ -c $$name.cov -v $$name.eigvec -V $$name.eigval
+
+%$(PROJSUFFIX) : %$(PROJSUFFIX).tmp
+	$(appendlastcol_command)
+
+%.eigval.tex : %.pca $(SCR)/plot_cumulative.py
+	$(SCR)/plot_cumulative.py $(basename $@)
 
 ## macros to be called later
 #MACROS +=
 
 ## info
 ifndef INFO
-INFO =
+INFO = select pca plot clean
+INFO_select = select inner columns
+INFO_pca   = dihedral principal component analysis
+INFO_plot   = plot cumulative variances
+INFO_clean  = delete cos-/sin-transformed data
 define INFOADD
 endef
 else
 INFOend +=
 endif
 
+## makefile includes (must remain after info)
+include $(makedir)/cossin.mk
+INFOend := $(filter-out cossin,${INFOend})
+
 ## keep intermediate files
-PRECIOUS +=
+PRECIOUS += $(INNERCOLSDATA)
 
 ## clean
-PLOTS_LIST +=
-CLEAN_LIST +=
-PURGE_LIST +=
+PLOTS_LIST += $(CVARPLOT)
+CLEAN_LIST += $(INNERCOLSDATA) $(addsuffix .tmp,${PCADATA})
+PURGE_LIST += $(foreach suffix,pca cov eigvec eigval,\
+	      $(addsuffix .${suffix},${INNERCOLSDATA}))
