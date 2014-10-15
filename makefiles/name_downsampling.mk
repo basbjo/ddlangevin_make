d8bc398a160338b63cff24b1dca3dd76120a5bc2
Bjoern Bastian <bjoern.bastian@physik.uni-freiburg.de>
2014-10-15 11:03:20 +0200

    downsampling: Separate makefile for filename creation

---
+++ makefiles/name_downsampling.mk
@@ -0,0 +1,16 @@
+# This makefile creates filenames for down sampled data.
+
+## variables
+SAMPDATA = $(foreach file,$(wildcard ${SAMPORIG})\
+	   ,$(foreach rfac,${REDUCTION_FACTORS}\
+	   ,$(call down_sampled_linkname,${file},${TIME_UNIT},${rfac})))
+
+## macros
+define down_sampled_linkname
+$(shell name=$(1); unit=$(2); factor=$(3);
+prefix=`echo $${name}|sed -r "s/_[0-9.]+$${unit}.*//"`;
+suffix=`echo $${name}|sed -r "s/.*_[0-9.]+$${unit}//"`;
+oldvalue=`echo $${name}|egrep -o "_[0-9.]+$${unit}($$|\.)"|grep -o '[0-9.]*[0-9]'`;
+newvalue=`echo $${oldvalue}*$${factor}|bc`;
+echo $${prefix}_$${newvalue}$${unit}$${suffix})
+endef
