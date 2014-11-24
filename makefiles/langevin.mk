0c58bcf534efa7a154609bee9947c4e7b2029330
Bjoern Bastian <bjoern.bastian@physik.uni-freiburg.de>
2014-11-24 14:20:44 +0100

    langevin: Rules to call olangevin testmodel programs

--- makefiles/langevin.mk
+++ makefiles/langevin.mk
@@ -16,12 +16,32 @@ $(1).%cols : $(1)
 	$(SCR)/select_outer_columns.awk -vnfirst=$$* -vnlast=$$(IF_FUTURE) $$< > $$@
 endef
 
-define rule_langevin
-$(foreach file,${DATA},$(eval $(call template_selectcolumns,${file})))
+define template_testmodel
+$(1).dle1.%.ltm: $(1)
+	ol-first$$(testmodel_command)
+$(1).dle2.%.ltm: $(1)
+	ol-second$$(testmodel_command)
+endef
+
+extract_argument = $(shell echo $@|egrep -o '\.$(1)[0-9]+\.'|grep -o '[0-9]*')
+
+define testmodel_command
+-tm$(if $(filter weights%,$*),-weights)\
+ -m$(call extract_argument,m) \
+ -k$(call extract_argument,k)$(if\
+$(shell [ ${IF_FUTURE} -eq 1 ] && echo yes),\
+ -F$(shell echo `expr $(call fcols,$<) + 1`))\
+ $< -o $@
 endef
 
 ## macros to be called later
 MACROS += rule_langevin
+
+define rule_langevin
+$(foreach file,${DATA},\
+	$(eval $(call template_selectcolumns,${file}))\
+	$(eval $(call template_testmodel,${file})))
+endef
 
 ## info
 ifndef INFO
@@ -30,6 +50,11 @@ define INFOADD
 
 To extract say 3 columns from a data file, call »make file.3cols«.
 If IF_FUTURE is 1, also the last column of the file is appended.
+
+Testmodel trajectories »file.dle<n>[.weights].m<m>.k<k>.ltm« can
+be created where <n> is 1 for ol-first-tm and 2 for ol-second-tm.
+The arguments to -m, -k and -F are generated automatically.  With
+».weights«, ol-first-tm-weights or ol-second-tm-weights is used.
 
 Save Langevin trajectories of a data file to »file.detail.lang«
 manually.  The detail commonly reflects the options, for options
