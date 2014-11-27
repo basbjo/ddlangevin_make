1bb5d7f7e85f3869c06bfa53cde323193c008050
Bjoern Bastian <bjoern.bastian@physik.uni-freiburg.de>
2014-11-27 12:15:29 +0100

    langevin: Data concatenation for dLE trajectories
    Concatenation of single-trajectory data in directory catdata
    to multi-trajectory data with follower column.

--- makefiles/langevin.mk
+++ makefiles/langevin.mk
@@ -1,21 +1,34 @@
-.PHONY:
+.PHONY: cat del_cat
+cat: $$(CAT_DATA)
+
+del_cat:
+	$(if $(wildcard ${CAT_DATA}),$(RM) $(wildcard ${CAT_DATA}))
 
 ## default settings
 
 # settings/data to be shown by showconf/showdata
 SHOWCONF += IF_FUTURE
-SHOWDATA +=
+SHOWDATA += catdir CAT_DATA
 
 ## default settings that must be changed before including this file
+catdir ?= catdata
 
 ## variables
+CAT_DATA = $(sort $(notdir $(shell echo $(wildcard ${catdir}/*-[0-9][0-9]) \
+		| tr ' ' '\n' | sed 's/-[0-9][0-9]$$//')))
+DIR_LIST += $(catdir)
 
 ## rules
+$(catdir):
+	mkdir -p $@
+
+# select columns from data files
 define template_selectcolumns
 $(1).%cols : $(1)
 	$(SCR)/select_outer_columns.awk -vnfirst=$$* -vnlast=$$(IF_FUTURE) $$< > $$@
 endef
 
+# create testmodel trajectories
 define template_testmodel
 $(1).dle1.%.ltm: $(1)
 	ol-first$$(testmodel_command)
@@ -34,18 +47,27 @@ $(shell [ ${IF_FUTURE} -eq 1 ] && echo yes),\
  $< -o $@
 endef
 
+# concatenate data files
+define template_cat
+$(1) : $$$$(wildcard $$$${catdir}/$(1)-[0-9]*[0-9])
+	$$(RM) $$@
+	for file in $$+; do sed '/^#/!s/ *$$$$/ 1/;$$$$s/ 1/ 0/' $$$${file} >> $$@; done
+endef
+
 ## macros to be called later
 MACROS += rule_langevin
 
 define rule_langevin
 $(foreach file,${DATA},\
 	$(eval $(call template_selectcolumns,${file}))\
-	$(eval $(call template_testmodel,${file})))
+	$(eval $(call template_testmodel,${file})))\
+$(foreach file,${CAT_DATA},\
+	$(eval $(call template_cat,${file})))
 endef
 
 ## info
 ifndef INFO
-INFO = split
+INFO = cat del_cat split
 define INFOADD
 
 To extract say 3 columns from a data file, call »make file.3cols«.
@@ -55,16 +77,21 @@ Testmodel trajectories »file.dle<n>[.weights].m<m>.k<k>.ltm« can
 be created where <n> is 1 for ol-first-tm and 2 for ol-second-tm.
 The arguments to -m, -k and -F are generated automatically.  With
 ».weights«, ol-first-tm-weights or ol-second-tm-weights is used.
+Testmodel trajectories can be splitted with the »split« target.
 
 Save Langevin trajectories of a data file to »file.detail.lang«
 manually.  The detail commonly reflects the options, for options
 »-m5« and »-k50« the results may be saved to »file.m5.k50.lang«.
 Edit »localconf.mk« to specify whether a follower column exists.
+Singe trajectories »catdata/file.detail.lang-##« are concatenated
+to »file.detail.lang« with follower column by the »cat« target.
 
 endef
 else
-INFOend +=
+INFOend += cat del_cat
 endif
+INFO_cat = concatenate data (directory $(patsubst ./%,%,${catdir}))
+INFO_del_cat = delete cat data
 
 ## keep intermediate files
 PRECIOUS +=
