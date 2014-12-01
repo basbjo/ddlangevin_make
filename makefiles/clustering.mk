32bf15d013128ccd633779a065bb4293cd94a3bd
Bjoern Bastian <bjoern.bastian@physik.uni-freiburg.de>
2014-12-01 15:23:44 +0100

    clustering: Plot cluster projections

--- makefiles/clustering.mk
+++ makefiles/clustering.mk
@@ -1,11 +1,15 @@
-.PHONY: clustering centers plot plot_all
+.PHONY: clustering centers plot plot_all plot_centers plot_projs
 clustering: $$(CLUSTER_TRAJ)
 
 centers: clustering $$(CLUSTER_CENTERS)
 
-plot: $$(CENTERS_PLOT)
+plot: plot_centers plot_projs
 
 plot_all: plot
+
+plot_centers: $$(CENTERS_PLOT)
+
+plot_projs: $$(PROJS_PLOT)
 
 ## default settings
 CLUSTER_LAST_COL ?= 5# last column (optional)
@@ -22,6 +26,8 @@ CLUSTER_TRAJ = $(addsuffix .clutraj,$(wildcard ${RAWDATA}))
 CLUSTER_CENTERS = $(addsuffix $(PROJSUFFIX).clucenters,$(wildcard ${RAWDATA}))
 CENTERS_PLOT = $(addprefix clucenters_,$(call add-V01-V02,\
 	       $(addsuffix $(PROJSUFFIX),$(wildcard ${RAWDATA})),.png,CLUSTER))
+PROJS_PLOT = $(addprefix cluprojs_,$(call add-V01-V02,\
+	       $(addsuffix $(PROJSUFFIX),$(wildcard ${RAWDATA})),.eps,CLUSTER))
 
 ## rules
 %.clutraj : % $(CLUSTER_SCRIPT)
@@ -43,7 +49,14 @@ endef
 define template_plot
 clucenters_$(1)-V$(2)-V$(3).tex : $(1).clucenters $$(SCR)/plot_cluster_centers.gp
 	gnuplot -e 'V1=$(patsubst 0%,%,${2}); V2=$(patsubst 0%,%,${3}); \
-		FILEROOT="$(1)"' $$(SCR)/plot_cluster_centers.gp
+		FILEROOT="$(1)"' $$(lastword $$+)
+endef
+
+define template_plot_alt
+cluprojs_$(1)$(PROJSUFFIX)-V$(2)-V$(3).eps : $(1)$(PROJSUFFIX) $(1).clutraj\
+		$$(SCR)/plot_cluster_projections.gp
+	gnuplot -e 'V1=$(patsubst 0%,%,${2}); V2=$(patsubst 0%,%,${3}); \
+		FILE="$$<"; CLUTRAJ="$$(word 2,$$+)"' $$(lastword $$+)
 endef
 
 ## macros to be called later
@@ -55,12 +68,14 @@ $(foreach file,$(wildcard ${RAWDATA}),\
 	$(foreach col2,$(call range,$(call getmin,${CLUSTER_LAST_COL}\
 		${lastcol})),$(foreach col1,$(call rangeto,${col2}),\
 		$(eval $(call template_plot\
-		,${file}${PROJSUFFIX},${col1},${col2})))))
+		,${file}${PROJSUFFIX},${col1},${col2}))\
+		$(eval $(call template_plot_alt\
+		,${file},${col1},${col2})))))
 endef
 
 ## info
 ifndef INFO
-INFO = clustering centers plot
+INFO = clustering centers plot_centers plot_projs plot
 define INFOADD
 endef
 else
@@ -68,7 +83,9 @@ INFOend +=
 endif
 INFO_clustering = apply clustering
 INFO_centers    = cluster centers and standard deviations
-INFO_plot       = plot cluster centers
+INFO_plot_centers = plot cluster centers
+INFO_plot_projs   = plot cluster projections
+INFO_plot         = create all plots
 
 ## keep intermediate files
 PRECIOUS +=
---
+++ makefiles/scripts/plot_cluster_projections.gp
@@ -0,0 +1,18 @@
+#!/usr/bin/gnuplot
+#Plots cluster projections
+
+if(!exists("CLUTRAJ")) CLUTRAJ=FILE.".clutraj"
+if(!exists("V1")) V1=1
+if(!exists("V2")) V2=2
+colselect = sprintf("<awk '{print $%d,$%d}' %s",V1,V2,FILE)
+cat = sprintf("| paste - %s",CLUTRAJ)
+exclude_no_man_s_land = "| awk '{if($3!=0)print $0}'"
+
+set terminal postscript eps color
+set output sprintf("cluprojs_%s-V%02d-V%02d.eps",FILE,V1,V2)
+
+set title sprintf("Clusters in %s",FILE)
+set xlabel sprintf('V%d',V1)
+set ylabel sprintf('V%d',V2)
+
+pl colselect.cat.exclude_no_man_s_land u 1:2:3 w points palette pt 5 notitle
