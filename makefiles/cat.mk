c413c8998d521875b4a2dee519f6410658a4d671
Bjoern Bastian <basbjo@gmail.com>
2014-12-02 11:48:10 +0100

    cat: Concatenate trajectories
    The functionality to concatenate single trajectories to
    one multi trajectory with column to denote followers is
    moved from langevin.mk to cat.mk.

--- makefiles/cat.mk
+++ makefiles/cat.mk
@@ -1,28 +1,73 @@
-.PHONY:
+.PHONY: cat del_cat
+cat: $$(CAT_DATA)
+
+del_cat:
+	$(if $(wildcard ${CAT_DATA}),$(RM) $(wildcard ${CAT_DATA}))
 
 ## default settings
 
 # settings/data to be shown by showconf/showdata
 SHOWCONF +=
-SHOWDATA +=
+SHOWDATA += catdir CAT_DATA
 
 ## default settings that must be changed before including this file
+catdir ?= catdata
 
 ## variables
+CAT_DATA = $(sort $(notdir $(shell echo $(wildcard ${catdir}/*-[0-9][0-9]) \
+		| tr ' ' '\n' | sed 's/-[0-9][0-9]$$//')))\
+	   $(sort $(notdir $(shell echo $(wildcard ${catdir}/*-[0-9][0-9].field) \
+		| tr ' ' '\n' | sed 's/-[0-9][0-9].field/.field/')))
+DIR_LIST += $(catdir)
 
 ## rules
+$(catdir):
+	mkdir -p $@
+
+define template_cat
+$(1) : $$$$(wildcard $$$${catdir}/$(1)-[0-9]*[0-9])
+	$$(cat_command)
+endef
+
+define cat_command
+$(RM) $@
+for file in $+; do sed '/^#/!s/ *$$/ 1/;$$s/ 1/ 0/' $${file} >> $@; done
+endef
+
+define template_cat_field
+$(1) : $$$$(wildcard $$$${catdir}/$(patsubst %.field,%,${1})-[0-9]*[0-9].field)
+	$$(cat_command)
+endef
 
 ## macros to be called later
-#MACROS +=
+MACROS += rule_cat
+
+define rule_cat
+$(foreach file,${DATA},\
+	$(eval $(call template_selectcolumns,${file}))\
+	$(eval $(call template_testmodel,${file})))\
+$(foreach file,$(filter-out %.field,${CAT_DATA}),\
+	$(eval $(call template_cat,${file})))\
+$(foreach file,$(filter %.field,${CAT_DATA}),\
+	$(eval $(call template_cat_field,${file})))
+endef
 
 ## info
 ifndef INFO
-INFO =
+INFO = cat
 define INFOADD
+
+Single trajectories »$(catdir)/filename-##« are concatenated
+to »filename« including a follower column by the »cat« target.
+Files with suffix ».field« are treated exceptionally: files
+»$(catdir)/name-##.field« are concatenated to »name.field«.
+
 endef
 else
-INFOend +=
+INFOend += cat del_cat
 endif
+INFO_cat = concatenate data (directory $(patsubst ./%,%,${catdir}))
+INFO_del_cat = delete concatenated data
 
 ## keep intermediate files
 PRECIOUS +=
@@ -30,4 +75,4 @@ PRECIOUS +=
 ## clean
 PLOTS_LIST +=
 CLEAN_LIST +=
-PURGE_LIST +=
+PURGE_LIST += $(CAT_DATA)
--- makefiles/langevin.mk
+++ makefiles/langevin.mk
@@ -1,29 +1,16 @@
-.PHONY: cat del_cat
-cat: $$(CAT_DATA)
-
-del_cat:
-	$(if $(wildcard ${CAT_DATA}),$(RM) $(wildcard ${CAT_DATA}))
+.PHONY:
 
 ## default settings
 
 # settings/data to be shown by showconf/showdata
 SHOWCONF += IF_FUTURE
-SHOWDATA += catdir CAT_DATA
+SHOWDATA +=
 
 ## default settings that must be changed before including this file
-catdir ?= catdata
 
 ## variables
-CAT_DATA = $(sort $(notdir $(shell echo $(wildcard ${catdir}/*-[0-9][0-9]) \
-		| tr ' ' '\n' | sed 's/-[0-9][0-9]$$//')))\
-	   $(sort $(notdir $(shell echo $(wildcard ${catdir}/*-[0-9][0-9].field) \
-		| tr ' ' '\n' | sed 's/-[0-9][0-9].field/.field/')))
-DIR_LIST += $(catdir)
 
 ## rules
-$(catdir):
-	mkdir -p $@
-
 # select columns from data files
 define template_selectcolumns
 $(1).%cols : $(1)
@@ -49,38 +36,12 @@ $(shell [ ${IF_FUTURE} -eq 1 ] && echo yes),\
  $< -o $@
 endef
 
-# concatenate data files
-define template_cat_lang
-$(1) : $$$$(wildcard $$$${catdir}/$(1)-[0-9]*[0-9])
-	$$(cat_command)
-endef
-
-define template_cat_field
-$(1) : $$$$(wildcard $$$${catdir}/$(patsubst %.field,%,${1})-[0-9]*[0-9].field)
-	$$(cat_command)
-endef
-
-define cat_command
-$(RM) $@
-for file in $+; do sed '/^#/!s/ *$$/ 1/;$$s/ 1/ 0/' $${file} >> $@; done
-endef
-
 ## macros to be called later
-MACROS += rule_langevin
-
-define rule_langevin
-$(foreach file,${DATA},\
-	$(eval $(call template_selectcolumns,${file}))\
-	$(eval $(call template_testmodel,${file})))\
-$(foreach file,$(filter-out %.field,${CAT_DATA}),\
-	$(eval $(call template_cat_lang,${file})))\
-$(foreach file,$(filter %.field,${CAT_DATA}),\
-	$(eval $(call template_cat_field,${file})))
-endef
+#MACROS +=
 
 ## info
 ifndef INFO
-INFO = cat del_cat split
+INFO = cat split
 define INFOADD
 
 To extract say 3 columns from a data file, call »make file.3cols«.
@@ -96,15 +57,14 @@ Save Langevin trajectories of a data file to »file.detail.lang«
 manually.  The detail commonly reflects the options, for options
 »-m5« and »-k50« the results may be saved to »file.m5.k50.lang«.
 Edit »localconf.mk« to specify whether a follower column exists.
-Singe trajectories »catdata/file.detail.lang-##« are concatenated
+Single trajectories »$(catdir)/file.detail.lang-##« are concatenated
 to »file.detail.lang« with follower column by the »cat« target.
+Files »$(catdir)/name-##.field« are concatenated to »name.field«.
 
 endef
 else
-INFOend += cat del_cat
+INFOend +=
 endif
-INFO_cat = concatenate data (directory $(patsubst ./%,%,${catdir}))
-INFO_del_cat = delete cat data
 
 ## keep intermediate files
 PRECIOUS +=
