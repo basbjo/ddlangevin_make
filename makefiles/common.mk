2a8a0aa5d885c3547575ff1a683be438817a923a
Bjoern Bastian <bjoern.bastian@physik.uni-freiburg.de>
2014-12-08 14:24:13 +0100

    common: Avoid help message of NCOLS in macro fcols

--- makefiles/common.mk
+++ makefiles/common.mk
@@ -214,7 +214,7 @@ getmax = $(shell echo ${1}|tr ' ' '\n'|sort -nr|head -1)
 # ceiling($1/$2) to find minimum integer x such that x*$2 >= $1
 divide_ceil = $(shell echo $$(((${1}-1+${2})/${2})))
 # columns in file $(1) minus $(IF_FUTURE)
-fcols = $(shell echo $$(($$(${NCOLS} ${1}) - ${IF_FUTURE})))
+fcols = $(shell echo $$(($$(${NCOLS} "${1}") - ${IF_FUTURE})))
 # range from 1 to $(1) in format "%02d"
 range = $(shell i=1; while [ "$(1)" != "" ] && [ $$i -le $(1) ]; do \
 	printf -- "%02d\n" $$i; i=`expr $$i + 1`; done)
