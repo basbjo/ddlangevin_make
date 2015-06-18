2ab68744ad90543d1dc67f9bca2f34e8ee9f2a31
Bjoern Bastian <bjoern.bastian@physik.uni-freiburg.de>
2015-06-18 10:38:18 +0200

    langevin: Settings for dLE and a new phony target

--- langevin/template/ol-second.mk
+++ langevin/template/ol-second.mk
@@ -1,33 +1,35 @@
 OLANGEVIN_DATA += $(addprefix catdata/,$(foreach suffix,-01 -02,\
 	$(addsuffix ${suffix},\
-	$(foreach k,25 200 500,\
+	$(foreach k,50 300 1000,\
 	$(RAWDATA).dle2$(OL_SUFFIX).m1.k${k}.lang))))
 #OLANGEVIN_DATA += $(addprefix catdata/,$(foreach suffix,-01 -02,\
 	$(addsuffix ${suffix},\
-	$(foreach k,750,\
+	$(foreach k,1500,\
 	$(RAWDATA).dle2$(OL_SUFFIX).weights.m1.k${k}.lang))))
 
+common_flags = -m1 -k$* -F$(shell echo `expr $(call fcols,$<) + 1`) -L1000001
+
 catdata/$(RAWDATA).dle2$(OL_SUFFIX).m1.k%.lang-01: $(RAWDATA)
-	ol-second$(OL_SUFFIX) -m1 -k$* -F7 -L1000001 -I1 $< -o $@
+	ol-second$(OL_SUFFIX) $(common_flags) -I1 $< -o $@
 
 catdata/$(RAWDATA).dle2$(OL_SUFFIX).m1.k%.lang-02: $(RAWDATA)
-	ol-second$(OL_SUFFIX) -m1 -k$* -F7 -L1000001 -I2 $< -o $@
+	ol-second$(OL_SUFFIX) $(common_flags) -I2 $< -o $@
 
 catdata/$(RAWDATA).dle2$(OL_SUFFIX).m1.k%.lang-03: $(RAWDATA)
-	ol-second$(OL_SUFFIX) -m1 -k$* -F7 -L1000001 -I3 $< -o $@
+	ol-second$(OL_SUFFIX) $(common_flags) -I3 $< -o $@
 
 catdata/$(RAWDATA).dle2$(OL_SUFFIX).m1.k%.lang-04: $(RAWDATA)
-	ol-second$(OL_SUFFIX) -m1 -k$* -F7 -L1000001 -I4 $< -o $@
+	ol-second$(OL_SUFFIX) $(common_flags) -I4 $< -o $@
 
 catdata/$(RAWDATA).dle2$(OL_SUFFIX).weights.m1.k%.lang-01: $(RAWDATA)
-	ol-second-weights$(OL_SUFFIX) -m1 -k$* -F7 -L1000001 -I1 $< -o $@
+	ol-second-weights$(OL_SUFFIX) $(common_flags) -I1 $< -o $@
 
 catdata/$(RAWDATA).dle2$(OL_SUFFIX).weights.m1.k%.lang-02: $(RAWDATA)
-	ol-second-weights$(OL_SUFFIX) -m1 -k$* -F7 -L1000001 -I2 $< -o $@
+	ol-second-weights$(OL_SUFFIX) $(common_flags) -I2 $< -o $@
 
 catdata/$(RAWDATA).dle2$(OL_SUFFIX).weights.m1.k%.lang-03: $(RAWDATA)
-	ol-second-weights$(OL_SUFFIX) -m1 -k$* -F7 -L1000001 -I3 $< -o $@
+	ol-second-weights$(OL_SUFFIX) $(common_flags) -I3 $< -o $@
 
 catdata/$(RAWDATA).dle2$(OL_SUFFIX).weights.m1.k%.lang-04: $(RAWDATA)
-	ol-second-weights$(OL_SUFFIX) -m1 -k$* -F7 -L1000001 -I4 $< -o $@
+	ol-second-weights$(OL_SUFFIX) $(common_flags) -I4 $< -o $@
 
