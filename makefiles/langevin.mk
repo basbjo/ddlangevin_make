7354f6d4ad7aa14fae82de2871f9ac7d17b582d0
Bjoern Bastian <bjoern.bastian@physik.uni-freiburg.de>
2015-06-24 17:08:26 +0200

    langevin: Update for option -t (test points)

--- makefiles/langevin.mk
+++ makefiles/langevin.mk
@@ -23,24 +23,15 @@ endef
 # create testmodel trajectories
 define template_testmodel
 $(1).dle1$(OL_SUFFIX).%.ltm: $(1)
-	ol-first$$(testmodel_command)
+	ol-first-tm$(OL_SUFFIX) $$(call testmodel_args,${1})
 $(1).dle2$(OL_SUFFIX).%.ltm: $(1)
-	ol-second$$(testmodel_command)
+	ol-second-tm$(OL_SUFFIX) $$(call testmodel_args,${1})
 $(1).dle1.%.osn: $(1)
-	ol-search-neighbors$(OL_SUFFIX) -W1 $$(m_k_opt_arg)$$(if\
-		$${OL_SN_FLAGS}, $${OL_SN_FLAGS})$$(if\
-		$$(wildcard ${1}.m$$(call extract_argument,m).osnp),\
-	-R) $$< $$(wildcard ${1}.m$$(call extract_argument,m).osnp) -o $$@
+	ol-search-neighbors$(OL_SUFFIX) -W1 $$(call neighbor_args,${1})
 $(1).dle2.%.osn: $(1)
-	ol-search-neighbors$(OL_SUFFIX) -W2 $$(m_k_opt_arg)$$(if\
-		$${OL_SN_FLAGS}, $${OL_SN_FLAGS})$$(if\
-		$$(wildcard ${1}.m$$(call extract_argument,m).osnp),\
-	-R) $$< $$(wildcard ${1}.m$$(call extract_argument,m).osnp) -o $$@
+	ol-search-neighbors$(OL_SUFFIX) -W2 $$(call neighbor_args,${1})
 $(1).%.osn: $(1)
-	ol-search-neighbors$(OL_SUFFIX) $$(m_k_opt_arg)$$(if\
-		$${OL_SN_FLAGS}, $${OL_SN_FLAGS})$$(if\
-		$$(wildcard ${1}.m$$(call extract_argument,m).osnp),\
-	-R) $$< $$(wildcard ${1}.m$$(call extract_argument,m).osnp) -o $$@
+	ol-search-neighbors$(OL_SUFFIX) $$(call neighbor_args,${1})
 endef
 
 extract_argument = $(shell echo $@|egrep -o '\.$(1)[0-9]+\.'|grep -o '[0-9]*')
@@ -50,12 +41,24 @@ define m_k_opt_arg
 -k$(call extract_argument,k)
 endef
 
-define testmodel_command
--tm$(OL_SUFFIX)\
+define testmodel_args
 $(m_k_opt_arg)$(if\
 $(shell [ ${IF_FUTURE} -eq 1 ] && echo yes),\
- -F$(shell echo `expr $(call fcols,$<) + 1`))\
- $(OL_TM_FLAGS) $< -o $@
+ -F$(shell echo `expr $(call fcols,$<) + 1`))$(if\
+$(wildcard $(1).m$(call extract_argument,m).ltmp),\
+ -t $(wildcard ${1}.m$(call extract_argument,m).ltmp))$(if\
+${OL_TM_FLAGS},\
+ ${OL_TM_FLAGS}) $< -o $@
+endef
+
+define neighbor_args
+$(m_k_opt_arg)$(if\
+$(shell [ ${IF_FUTURE} -eq 1 ] && echo yes),\
+ -F$(shell echo `expr $(call fcols,$<) + 1`))$(if\
+$(wildcard $(1).m$(call extract_argument,m).osnp),\
+ -t $(wildcard ${1}.m$(call extract_argument,m).osnp))$(if\
+${OL_SN_FLAGS},\
+ ${OL_SN_FLAGS}) $< -o $@
 endef
 
 ## macros to be called later
@@ -85,6 +88,8 @@ Testmodel:
   can be created where <n> is 1 for ol-first-tm$(OL_SUFFIX)
                           and 2 for ol-second-tm$(OL_SUFFIX).
   Correct arguments to -m, -k and -F are generated automatically.
+  If »symlink.m<m>.ltmp« is provided, fields are calculated for
+  points in this file and not for all points in input data.
   Additional options can be passed to the »OL_TM_FLAGS« variable.
   Testmodel trajectories can be splitted with the »split« target.
   Use variable OL_TM_FLAGS to provide additional options.
@@ -95,7 +100,7 @@ Neighbors:
   ol-second, create »symlink.dle<n>.m<m>.k<k>.osn«, n = 1,2.
   If »symlink.m<m>.osnp« is provided, neighbors are searched for
   points in this file and not for all points in input data.
-  Use variable OL_SN_FLAGS to provide additional options.
+  Use variable »OL_SN_FLAGS« to provide additional options.
 
 Langevin:
   Save Langevin trajectories of a data file to »file.detail.lang«
