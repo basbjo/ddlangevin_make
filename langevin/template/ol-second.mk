89e7e9b7acb46e9e435e1407386673aa47990d95
Bjoern Bastian <bjoern.bastian@physik.uni-freiburg.de>
2015-07-01 14:32:41 +0200

    langevin: Create directory catdata if missing

--- langevin/template/ol-second.mk
+++ langevin/template/ol-second.mk
@@ -9,27 +9,27 @@ OLANGEVIN_DATA += $(addprefix catdata/,$(foreach suffix,-01 -02,\
 
 common_flags = -m1 -k$* -F$(shell echo `expr $(call fcols,$<) + 1`) -L1000001
 
-catdata/$(RAWDATA).dle2$(OL_SUFFIX).m1.k%.lang-01: $(RAWDATA)
+catdata/$(RAWDATA).dle2$(OL_SUFFIX).m1.k%.lang-01: $(RAWDATA) | catdata
 	ol-second$(OL_SUFFIX) $(common_flags) -I1 $< -o $@
 
-catdata/$(RAWDATA).dle2$(OL_SUFFIX).m1.k%.lang-02: $(RAWDATA)
+catdata/$(RAWDATA).dle2$(OL_SUFFIX).m1.k%.lang-02: $(RAWDATA) | catdata
 	ol-second$(OL_SUFFIX) $(common_flags) -I2 $< -o $@
 
-catdata/$(RAWDATA).dle2$(OL_SUFFIX).m1.k%.lang-03: $(RAWDATA)
+catdata/$(RAWDATA).dle2$(OL_SUFFIX).m1.k%.lang-03: $(RAWDATA) | catdata
 	ol-second$(OL_SUFFIX) $(common_flags) -I3 $< -o $@
 
-catdata/$(RAWDATA).dle2$(OL_SUFFIX).m1.k%.lang-04: $(RAWDATA)
+catdata/$(RAWDATA).dle2$(OL_SUFFIX).m1.k%.lang-04: $(RAWDATA) | catdata
 	ol-second$(OL_SUFFIX) $(common_flags) -I4 $< -o $@
 
-catdata/$(RAWDATA).dle2$(OL_SUFFIX).weights.m1.k%.lang-01: $(RAWDATA)
+catdata/$(RAWDATA).dle2$(OL_SUFFIX).weights.m1.k%.lang-01: $(RAWDATA) | catdata
 	ol-second-weights$(OL_SUFFIX) $(common_flags) -I1 $< -o $@
 
-catdata/$(RAWDATA).dle2$(OL_SUFFIX).weights.m1.k%.lang-02: $(RAWDATA)
+catdata/$(RAWDATA).dle2$(OL_SUFFIX).weights.m1.k%.lang-02: $(RAWDATA) | catdata
 	ol-second-weights$(OL_SUFFIX) $(common_flags) -I2 $< -o $@
 
-catdata/$(RAWDATA).dle2$(OL_SUFFIX).weights.m1.k%.lang-03: $(RAWDATA)
+catdata/$(RAWDATA).dle2$(OL_SUFFIX).weights.m1.k%.lang-03: $(RAWDATA) | catdata
 	ol-second-weights$(OL_SUFFIX) $(common_flags) -I3 $< -o $@
 
-catdata/$(RAWDATA).dle2$(OL_SUFFIX).weights.m1.k%.lang-04: $(RAWDATA)
+catdata/$(RAWDATA).dle2$(OL_SUFFIX).weights.m1.k%.lang-04: $(RAWDATA) | catdata
 	ol-second-weights$(OL_SUFFIX) $(common_flags) -I4 $< -o $@
 
